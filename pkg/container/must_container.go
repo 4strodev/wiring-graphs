fd85975706e0ca55fa862393735c52dec27cbe52
4strodev <jumarurex@gmail.com>
2025-08-01 17:04:54 +0200

    feat: add must contianer

---
+++ pkg/container/must_container.go
@@ -0,0 +1,43 @@
+package container
+
+type MustContainer struct {
+	container *Container
+}
+
+func (c *MustContainer) TokenSingleton(dependencies map[string]any) *MustContainer {
+	err := c.container.TokenSingleton(dependencies)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
+
+func (c *MustContainer) Singleton(resolvers ...any) *MustContainer {
+	err := c.container.Singleton(resolvers)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
+
+func (c *MustContainer) Token(dependencies map[string]any) *MustContainer {
+	err := c.container.Token(dependencies)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
+
+func (c *MustContainer) Dependencies(resolvers ...any) *MustContainer  {
+	err := c.container.Dependencies(resolvers)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
+
+
